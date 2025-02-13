b4218e3ad08c6a2b5039819326618f8661b74092
WalterMarch <walter.march@outlook.com>
2025-02-13 09:47:05 +0000

    add choose random section name

--- apiRequests/apiRequests.java
+++ apiRequests/apiRequests.java
@@ -2,9 +2,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Random;
 
 public class apiRequests {
     public static void main(String[] args) {
+        String newSection = getNewsSection();
+        System.out.println(newSection);
+    }
+
+    public static String getNewsSection() {
         try {
             HttpClient client = HttpClient.newHttpClient();
 
@@ -14,11 +20,19 @@ public class apiRequests {
                 .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            String[] trimmedResponseBody = responseBody.substring(1, responseBody.length() - 2).split(",");
 
-            System.out.println("Status Code: " + response.statusCode());
-            System.out.println("Response Body: " + response.body());
+            Random random = new Random();
+            int index = random.nextInt(trimmedResponseBody.length);
+
+            String sectionName = trimmedResponseBody[index];
+            String trimmedSectionName = sectionName.substring(1, sectionName.length() - 1);
+
+            return trimmedSectionName;
         } catch (Exception e) {
             e.printStackTrace();
+            return "";
         }
     }
 }
